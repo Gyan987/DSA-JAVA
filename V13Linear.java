549abc0692559278fb49aa2aac740478bfcf911a
Gyan <gyanprakasht987@gmail.com>
2026-05-17 22:01:55 +0530

    add 3 more functions

--- V13Linear.java
+++ V13Linear.java
@@ -2,7 +2,11 @@
 
 import java.util.*;
 public class V13Linear {
-    public static void main(String[] args){
+
+
+
+
+    void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int[] num;
         System.out.print("Enter the size of the array :");
@@ -16,10 +20,38 @@ public class V13Linear {
         int target;
         System.out.print("Enter the target element :");
         target=sc.nextInt();
-        int ans=linearsearch(num,target);//function call .
+        
+        int ans=linearsearch2(num,target);//function call(1,2,3).
         System.out.println("Index of the target element " + target + " is: " + ans);
     }
 
+
+
+// function-3
+       static int linearsearch3(int[]arr,int target){
+        if(arr.length==0){
+            return false;
+        }
+        for(int elements:arr){
+            if(elements==target){
+                return true;
+            }
+        }
+        return false;
+    }
+//function-2
+    static int linearsearch2(int[]arr,int target){
+        if(arr.length==0){
+            return Integer.MAX_VALUE;
+        }
+        for(int elements:arr){
+            if(elements==target){
+                return elements;
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+// function-1
     static int linearsearch(int[]arr,int target){
         if(arr.length==0){
             return -1;
